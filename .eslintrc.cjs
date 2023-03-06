b481e3bf24bbfcefaf280672029869e9bdd984fd
jingll <the2ndindec@hotmail.com>
2023-03-06 10:28:18 +0800

    🎈 perf:
    multi-word-component-names 规则排除index命名的文件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,5 +19,12 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['index'],
+      },
+    ],
+  },
 }
