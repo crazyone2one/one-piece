2064ef7cd6e5641a9a87989761bea7d749e4ae52
jingll <the2ndindec@hotmail.com>
2023-03-07 15:16:50 +0800

    🐞 fix(i18n):
    fix warn on the console "You are running ..."

--- vite.config.ts
+++ vite.config.ts
@@ -95,6 +95,9 @@ export default defineConfig(({ mode }) => {
     build: {},
     define: {
       __APP_ENV__: env.APP_ENV,
+      __VUE_I18N_FULL_INSTALL__: true,
+      __VUE_I18N_LEGACY_API__: false,
+      __INTLIFY_PROD_DEVTOOLS__: false,
     },
   }
 })
